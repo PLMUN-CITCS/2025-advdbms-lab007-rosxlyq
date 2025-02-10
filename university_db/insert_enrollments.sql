cc63df6863f19b2c3501f534be217eb891d8cc20
rosxlyq <deveyrarosalyn05@gmail.com>
2025-02-10 18:49:10 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- university_db/insert_enrollments.sql
+++ university_db/insert_enrollments.sql
@@ -1,4 +1,6 @@
 USE `UniversityDB`;
+
+-- Step 2: Insert data into enrollments table
 INSERT INTO `Enrollments` (`StudentID`, `CourseID`, `EnrollmentDate`)
 VALUES
    (1, 1, '2023-09-01'),
@@ -6,3 +8,4 @@ VALUES
    (1, 3, '2023-09-01'),
    (2, 4, '2023-09-02'),
    (2, 5, '2023-09-03');
+
